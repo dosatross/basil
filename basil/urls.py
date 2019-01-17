a3aff6f00537771f904554fce485129177a51810
hayden warmington <haydenwarmington@gmail.com>
2019-01-17 18:40:36 +1100

    api docs

--- basil/urls.py
+++ basil/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('docs/', include_docs_urls(title='Basil API Docs')),
 ]
 
 
