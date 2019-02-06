20b0e3531dfdbfe663c86f01fa0d3a6eaa811e55
hayden warmington <haydenwarmington@gmail.com>
2019-02-06 23:26:13 +1100

    make categorygroup name unique

--- basil/apps/categories/models.py
+++ basil/apps/categories/models.py
@@ -3,7 +3,7 @@ from django.db.models import Q
 from django.contrib.auth.models import User
 
 class CategoryGroup(models.Model):
-	name = models.CharField(max_length=50)
+	name = models.CharField(max_length=50,unique=True)
 	description = models.CharField(max_length=150, null=True)
 
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
