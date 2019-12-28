460ad970eaf16f0cb12a0891eca7c0b5ff29b8aa
hayden warmington <haydenwarmington@gmail.com>
2019-12-29 10:24:17 +1100

    root query

--- basil/schema.py
+++ basil/schema.py
@@ -1,17 +1,29 @@
 import logging
-import graphene
+from graphene import Schema, ObjectType
+import graphql_jwt
 from graphene_django import DjangoObjectType
-from django.contrib.auth import models
+from basil.apps.accounts.api.graphql import UserProfileQuery
+from basil.apps.transactions.api.graphql import (TransactionQuery, DateRangeQuery, 
+    PeriodTotalQuery, CategoryTotalQuery, PeriodCategoryTotalQuery, CategoryPeriodTotalQuery)
+from basil.apps.categories.api.graphql import CategoryQuery, CategoryGroupQuery
 
-class UserType(DjangoObjectType):
-    class Meta:
-         model = models.User
 
-class Query(graphene.ObjectType):
-    user = graphene.Field(UserType)
+class Query(UserProfileQuery,
+            TransactionQuery,
+            CategoryQuery,
+            CategoryGroupQuery,
+            DateRangeQuery, 
+            PeriodTotalQuery, 
+            CategoryTotalQuery, 
+            PeriodCategoryTotalQuery, 
+            CategoryPeriodTotalQuery, 
+            ObjectType):
+  pass
+    
 
-    def resolve_user(self, info, **kwargs):
-      user = info.context.user
-      return user
+class Mutation(ObjectType):
+  token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+  verify_token = graphql_jwt.Verify.Field()
+  refresh_token = graphql_jwt.Refresh.Field()
 
-schema = graphene.Schema(query=Query)
+schema = Schema(query=Query, mutation=Mutation)
