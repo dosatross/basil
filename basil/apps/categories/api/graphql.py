39d2be29c68b985bc551238c5245ca6a6064c1c5
hayden warmington <haydenwarmington@gmail.com>
2020-03-28 17:19:08 +1100

    gql optimizer to avoid n+1 sql queries

--- basil/apps/categories/api/graphql.py
+++ basil/apps/categories/api/graphql.py
@@ -3,6 +3,7 @@ from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
 from graphene_django.rest_framework.mutation import SerializerMutation
 from graphene_django_extras import DjangoSerializerMutation
+import graphene_django_optimizer as gql_optimizer
 
 from basil.utils.graphql import Connection
 from basil.apps.categories.models import Category, CategoryGroup
@@ -14,15 +15,12 @@ class CategoryConnection(Connection):
   class Meta:
     abstract = True
 
-  def resolve_all(self, info, **kwargs):
-    return Category.objects.filter(user=info.context.user)
-
 class CategoryType(DjangoObjectType):
   is_credit = Field(Int,required=False)
   class Meta:
     model = Category
     interfaces = (relay.Node, )
-    filter_fields  = []
+    filter_fields = []
     connection_class = CategoryConnection
   
   @classmethod
@@ -34,7 +32,7 @@ class CategoryQuery(ObjectType):
 
   def resolve_category_connection(self, info, category_set=None, **kwargs):
     qs = Category.objects.filter(user=info.context.user)
-    return filter_categories_by_category_set(qs,category_set)
+    return gql_optimizer.query(filter_categories_by_category_set(qs,category_set),info)
 
 class CategoryMutation(DjangoSerializerMutation):
 
@@ -53,14 +51,11 @@ class CategoryGroupConnection(Connection):
   class Meta:
     abstract = True
 
-  def resolve_all(self, info, **kwargs):
-    return CategoryGroup.objects.filter(user=info.context.user)
-
 class CategoryGroupType(DjangoObjectType):
   class Meta:
     model = CategoryGroup
     interfaces = (relay.Node, )
-    filter_fields  = []
+    filter_fields = []
     connection_class = CategoryGroupConnection
   
   @classmethod
@@ -71,7 +66,7 @@ class CategoryGroupQuery(ObjectType):
   category_group_connection = DjangoFilterConnectionField(CategoryGroupType)
 
   def resolve_category_group_connection(self, info, **kwargs):
-    return CategoryGroup.objects.filter(user=info.context.user)
+    return gql_optimizer.query(CategoryGroup.objects.filter(user=info.context.user),info)
 
 class CategoryGroupMutation(DjangoSerializerMutation):
 
